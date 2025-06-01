a947f7f2c575c3ad6f749acf32994f2c3b802543
tresor-del <tresoradzanto7@gmail.com>
2025-06-01 07:05:44 +0000

    init

---
+++ equation.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float a, b, c;
+    float  discriminant, x0, x1, x2;
+
+    printf("Donnez les valeurs de a, b et c\n");
+
+    printf("a: ");
+    scanf("%f", &a);
+
+    printf("b: ");
+    scanf("%f", &b);
+
+    printf("c: ");
+    scanf("%f", &c);
+
+    printf("L'équation est %.2fx² + %.2fx + %.2f \n", a, b, c);
+
+    discriminant = b*b - 4*a*c;
+
+    printf("Le discriminant donne: %f\n", discriminant);
+
+    if (discriminant == 0)
+    {
+        x0 = - b / (2*a);
+
+        printf("L'équation a une seule solution: \n");
+        printf("x0: %.3f\n", x0);
+    } else if (discriminant<0)
+    {
+        printf("L'équation n'a pas de solution");
+    } else
+    {
+        x1 = (b + sqrt(discriminant)) / (2*a);
+        x2 = (-b + sqrt(discriminant)) / (2*a);
+
+        printf("L'équation a deux solutions: \n");
+        printf("x1: %.3f\n", x1);
+        printf("x2: %.3f\n", x2);
+    }
+    
+    
+    
+    
+}
